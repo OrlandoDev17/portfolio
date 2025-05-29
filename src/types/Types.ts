efdfbf8883f67424829d58701582be5ba6810620
OrlandoDev17 <orlandodev1707@gmail.com>
2025-05-29 17:01:06 -0400

    feat: Añadir interfaz Project con propiedades y estructura actualizadas

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -9,3 +9,15 @@ export interface SocialLink {
   icon: any;
   color: string;
 }
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  links?: {
+    demo: string;
+    code: string;
+  }[]; // Cambiado a array
+  tags?: { icon: any; color?: string; name?: string }[];
+}
