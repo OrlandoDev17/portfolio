ee802c7df7c09552d3ca1f1523de635d9ddee760
OrlandoDev17 <orlandodev1707@gmail.com>
2025-06-12 17:13:24 -0400

    fix: Corregir tipos de props para el icono en SectionContainer y mejorar la estructura del componente

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 interface sectionContainerProps {
   className?: string;
   id?: string;
-  icon?: React.ComponentType;
+  icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   title?: string;
   paragraph?: string;
   children?: ReactNode;
@@ -18,13 +18,18 @@ export default function SectionContainer({
   children,
 }: sectionContainerProps) {
   return (
-    <section id={id} className={`${className}`}>
-      <div>
-        <h2>
-          {Icon && <Icon />}
+    <section
+      id={id}
+      className={`flex flex-col items-center justify-center px-8 md:px-16 xl:px-32 2xl:px-64 py-14 ${className}`}
+    >
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h2 className="flex items-center gap-2 text-4xl font-semibold dark:text-white">
+          {Icon && <Icon className="text-blue-500 size-12" />}
           {title}
         </h2>
-        <p>{paragraph}</p>
+        <p className="max-w-3xl text-center text-lg text-gray-700 dark:text-gray-300">
+          {paragraph}
+        </p>
       </div>
       {children}
     </section>
